789715672e2d4b9b8643fa911721f080b489233c
CreeO618 <christ.zeman@gmail.com>
2024-08-21 19:29:26 +0200

    change the ičo to ico

--- curl_sidla.py
+++ curl_sidla.py
@@ -185,8 +185,11 @@ def main():
             file_name="ico_sidla_ke_kontrole.csv",
             mime="text/csv"
         )
-        
-        csv_ares_modified = df_ares_modified.to_csv(index=False)
+        # in df_ares_modified change the "IČO" to "ICO" 
+        df_ares_modifiedtodownload = df_ares_modified.copy()
+        df_ares_modifiedtodownload = re.sub(r'IČO', 'ICO', df_ares_modifiedtodownload)
+        df_ares_modifiedtodownload
+        csv_ares_modified = df_ares_modifiedtodownload.to_csv(index=False)
         st.download_button(
             label="Stáhnout Ares data do CSV",
             data=csv_ares_modified ,
