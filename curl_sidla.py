c5aad6e8ee2ab806894ee90b1e4baea9bc01dd44
CreeO618 <christ.zeman@gmail.com>
2024-08-23 12:43:10 +0200

    colors last try

--- curl_sidla.py
+++ curl_sidla.py
@@ -205,9 +205,9 @@ def main():
         st.write(f"Celkem IČO v datech z Aresu: {len(df_ares_modified)}")
         st.write(f":blue[IČO nalezené v Aresu] ale NE v posledním souboru (pravděpodovně nové sídlo): {len(ico_in_api_not_in_csv)}")
         st.write(f":red[IČO nenalezené v Aresu] (pravděpodobně zrušeno sídlo): {len(ico_in_original_not_in_api)}")
-        st.subheader("Sídla pravděpodobně nová - NEnalezena v nahraném souboru")
+        st.subheader("Sídla pravděpodobně nová - nalezena v Aresu ale ne v našem souboru")
         st.dataframe(ico_in_api_not_in_csv.head(n=30))
-        st.subheader("Sídla pravděpodobně zrušená - NEnalezena v Aresu")
+        st.subheader("Sídla pravděpodobně zrušená - :red[Nenalezena] v Aresu ale v naše souboru jsou")
         st.dataframe(ico_in_original_not_in_api.head(n=30))
         
 
